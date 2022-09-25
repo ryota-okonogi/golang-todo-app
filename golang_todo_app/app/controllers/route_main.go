1c61b41cc54df61578612965f1b51a22e9bd68d7
nogi5000 <R5000.power@gmail.com>
2022-09-25 13:26:29 +0900

    signup機能の実装

--- golang_todo_app/app/controllers/route_main.go
+++ golang_todo_app/app/controllers/route_main.go
@@ -6,5 +6,5 @@ import (
 
 // ハンドラー
 func top(w http.ResponseWriter, r *http.Request) {
-	generateHTML(w, "Hello", "layout", "top") //メソッド(args1: ResponseWriter, args2: data[明示的に渡している], args3: template[template1, template2])
+	generateHTML(w, "Hello", "layout", "public_navbar", "top") //メソッド(args1: ResponseWriter, args2: data[明示的に渡している], args3: template[template1, template2])
 }
--- golang_todo_app/app/controllers/server.go
+++ golang_todo_app/app/controllers/server.go
@@ -23,6 +23,7 @@ func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
 	http.Handle("/static/", http.StripPrefix("/static", files)) //args1: URLPath, args2: files{ ("/static", files)にするのはstaticをPathの階層から取り除くため }
 
-	http.HandleFunc("/", top)                               //関数名(第１引数：URL, 第２引数：ハンドラー)
+	http.HandleFunc("/", top) //関数名(第１引数：URL, 第２引数：ハンドラー)
+	http.HandleFunc("/signup", signup)
 	return http.ListenAndServe(":"+config.Config.Port, nil) //(パッケージ名.変数名.フィールド)
 }
